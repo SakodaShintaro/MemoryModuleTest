e1b77bdd39725e8c20c52465694f585263e20de8
sakoda <sakoda@keio.jp>
2020-07-07 16:28:29 +0900

    実装をlstm.cppに分離

--- lstm.hpp
+++ lstm.hpp
@@ -5,41 +5,13 @@
 
 class LSTMImpl : public torch::nn::Module {
 public:
-    LSTMImpl(int64_t input_size, int64_t output_size, int64_t num_layers = 1, int64_t hidden_size = 512) :
-    input_size_(input_size),
-    num_layers_(num_layers),
-    hidden_size_(hidden_size) {
-        torch::nn::LSTMOptions option(input_size, hidden_size);
-        option.num_layers(num_layers);
-        lstm_ = register_module("lstm_", torch::nn::LSTM(option));
-        final_layer_ = register_module("final_layer_", torch::nn::Linear(hidden_size, output_size));
-        resetState();
-    }
+    LSTMImpl(int64_t input_size, int64_t output_size, int64_t num_layers = 1, int64_t hidden_size = 512);
 
-    torch::Tensor forward(const torch::Tensor& x) {
-        //lstmは入力(input, (h_0, c_0))
-        //inputのshapeは(seq_len, batch, input_size)
-        //h_0, c_0は任意の引数で、状態を初期化できる
-        //h_0, c_0のshapeは(num_layers_ * num_directions, batch, hidden_size_)
-        //出力はoutput, (h_n, c_n)
+    //入力を受けて1ステップ分LSTMの推論を進める関数:これを直に触って使うのはやめた方が良さそう
+    torch::Tensor forward(const torch::Tensor& x);
 
-        //実践的に入力は系列を1個ずつにバラしたものが入るのでshapeは(1, input_size_)
-        //まずそれを直す
-        torch::Tensor input = x.view({ 1, 1, input_size_ });
-
-        //outputのshapeは(seq_len, batch, num_directions * hidden_size)
-        auto[output, h_and_c] = lstm_->forward(input, std::make_tuple(h_, c_));
-        std::tie(h_, c_) = h_and_c;
-
-        output = final_layer_->forward(output);
-
-        return output;
-    }
-
-    void resetState() {
-        h_ = torch::zeros({ num_layers_, 1, hidden_size_ });
-        c_ = torch::zeros({ num_layers_, 1, hidden_size_ });
-    }
+    //内部状態をリセットする関数:上のforwardを使わないようにできれば自然とこの関数も不要になる
+    void resetState();
 private:
     int64_t input_size_;
     int64_t num_layers_;
