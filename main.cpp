b68cbe17f83a229d8545a18110c30948546f777d
sakoda <sakoda@keio.jp>
2020-07-08 10:31:56 +0900

    タスクを入力によって選択できるように変更

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include "task.hpp"
+#include <string>
+#include <iostream>
+#include <map>
+#include <functional>
 
 int main() {
-    treeTask();
-    //echoTask();
-    //treeTaskBatch();
+    std::string task_name;
+    std::cout << "タスク名: ";
+    std::cin >> task_name;
+
+    std::map<std::string, std::function<void()>> mp = {
+            { "echo", echoTask },
+            { "tree", treeTask },
+            { "treeBatch", treeTaskBatch }
+    };
+
+    for (const auto&[name, func] : mp) {
+        if (name == task_name) {
+            func();
+            return 0;
+        }
+    }
+
+    std::cout << "Error : 実装されているタスク一覧" << std::endl;
+    for (const auto& p : mp) {
+        std::cout << p.first << std::endl;
+    }
 }
